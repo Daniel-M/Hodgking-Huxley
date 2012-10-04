40dcbb0ad1a3f9a13896eb106e75c53a8ef6e238
Daniel-M <danielmejia55@gmail.com>
2012-10-04 12:49:22 +0000

    Need to fix the graphics naming on the axis

--- src/HH_GatingFunctions.hpp
+++ src/HH_GatingFunctions.hpp
@@ -1,39 +1,11 @@
-/*
-    FILE NAME: HH_GatingFunctions.hpp
-  DESCRIPTION: Part of Hodgking-Huxley model, used in HH-model.cpp
-
-       AUTHOR: Daniel Mejia Raigosa
-       E-MAIL: danielmejia55@gmail.com
-       GITHUB: http://github.com/Daniel-M/Hodgking-Huxley
-         DATE: 13 Sept 2012
-      VERSION: 1.0
-    
-    LICENSE
-    
-    This file is part of "Hodgking-Huxley".
-
-    "HH_GatingFunctions.hpp" is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    "HH_GatingFunctions.hpp" is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with "HH_GatingFunctions.hpp".  If not, see <http://www.gnu.org/licenses/>.
-    
-    
-*/
+//#include "HH_Includes.hpp"
 
 
 // ===============================================================================
-//  Defining some auxiliary "gating functions" for the Hodgking Huxley Model
-//  These are taken from book "Nonlinear dynamics in physiology and medicine"
-//  From Anne Beuter et al.
-//  Chapter 4 - Excitable cells, pp 104
+// Defining some auxiliary "gating functions" for the Hodgking Huxley Model
+// These are taken from book "Nonlinear dynamics in physiology and medicine"
+// From Anne Beuter et al.
+// Chapter 4 - Excitable cells, pp 104
 // ===============================================================================
 
 double alpha_n(double V)
@@ -70,41 +42,43 @@ double beta_h(double V)
 
 
 // ===============================================================================
-//  Defining some auxiliary "gating functions" for the Hodgking Huxley Model
-//  These are taken from book "Mathematical aspects of the Hodgking-Huxley
-//  Neural theory"
-//  From Janne Cronin
-//  Chapter 2.3 - The work of Hodgking-Huxley, pp 50-52
+// Defining some auxiliary "gating functions" for the Hodgking Huxley Model
+// These are taken from book "Mathematical aspects of the Hodgking-Huxley
+// Neural theory"
+// From Janne Cronin
+// Chapter 2.3 - The work of Hodgking-Huxley, pp 50-52
 // ===============================================================================
 /*
 double alpha_n(double V)
 {
-     return (0.01*(V+10.0))/(exp((V+10.0)/10.0)-1.0);
+return (0.01*(V+10.0))/(exp((V+10.0)/10.0)-1.0);
 }
 
 double beta_n(double V)
 {
-     return 0.125*exp(V/80.0);
+return 0.125*exp(V/80.0);
 }
 
 
 double alpha_m(double V)
 {
-     return (0.1*(V+25.0))/(exp((V+25.0)/10.0)-1.0);
+return (0.1*(V+25.0))/(exp((V+25.0)/10.0)-1.0);
 }
 
 double beta_m(double V)
 {
-     return 4.0*exp(V/18.0);
+return 4.0*exp(V/18.0);
 }
 
 
 double alpha_h(double V)
 {
-     return 0.07*exp(V/20.0);
+return 0.07*exp(V/20.0);
 }
 
 double beta_h(double V)
 {
-     return 1.0/(1.0+exp((V+30.0)/10.0));
+return 1.0/(1.0+exp((V+30.0)/10.0));
 }*/
+
+
--- src/getInfoFromFile.hpp
+++ src/getInfoFromFile.hpp
@@ -1,47 +1,32 @@
 /*
-    FILE NAME: getInfoFromFile.hpp
-  DESCRIPTION: Part of Hodgking-Huxley model, used in HH-model.cpp
+	FILE NAME: HH-model.cpp
+	DESCRIPTION: This code uses GSL to calculate the solution to the Hodgking Huxley Model
 
-       AUTHOR: Daniel Mejia Raigosa
-       E-MAIL: danielmejia55@gmail.com
-       GITHUB: http://github.com/Daniel-M/Hodgking-Huxley
-         DATE: 13 Sept 2012
-      VERSION: 1.0
-    
-    LICENSE
-    
-    This file is part of "Hodgking-Huxley".
+	[!] The system must be normalized to get the feeling of the biophysical behavior
+	[!] See the txt files to more info about the model
+	AUTHOR: Daniel Mejia Raigosa
+	E-MAIL: danielmejia55@gmail.com
+	GITHUB: http://github.com/Daniel-M/
+	DATE: 13 Sept 2012
+	VERSION: 1.0
 
-    "getInfoFromFile.hpp" is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	LICENSE
+	This file is part of "Hodgking-Huxley".
 
-    "getInfoFromFile.hpp" is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	"HH-model.cpp" is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with "getInfoFromFile.hpp".  If not, see <http://www.gnu.org/licenses/>.
-    
-    
+	"HH-model.cpp" is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with "HH-model.cpp". If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef IOSTREAM_H
-    #include <iostream>
-    #define IOSTREAM_H
-#endif 
-
-#ifndef  SSTRING_H
-  #include <string>
-  #define SSTRING_H
-#endif
-
-#ifndef FSTREAM_H
-    #include<fstream>
-    #define FSTREAM_H
-#endif
 
 
 void getInfoFromFile(std::string filename, double buffer[])
@@ -55,15 +40,11 @@ void getInfoFromFile(std::string filename, double buffer[])
   
     while(data.eof()!=true)
     {
-      data.getline(sBuffer,20);
-
-      if(sBuffer[0]!='#')
-      {
-        buffer[i]=atof(sBuffer);
-        i++;
-      }
-      
+data.getline(sBuffer,20);
+buffer[i]=atof(sBuffer);
+i++;
     }
-
+   
        data.close();
 }
+
